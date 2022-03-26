e020d0e7eb3e601e132217b17c3392030190619b
xxxDeveloper <humengmingxx@gmail.com>
2022-03-26 10:00:29 +0800

    chore(project)：init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ts-tmpl
+
+> 纯 TypeScript 项目的启动模版
---
+++ test/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, assert, it } from 'vitest'
+
+describe('should', () => {
+  it('exported', () => {
+    assert.equal('xxx', 'xxx')
+  })
+})
